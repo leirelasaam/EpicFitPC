734f48a1cfb568620656feb2b6200cdffe663f22
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2024-10-19 11:34:45 +0200

    Eliminar fallo de prueba

--- src/main/java/epicfitpc/EpicFitPC.java
+++ src/main/java/epicfitpc/EpicFitPC.java
@@ -1,7 +1,7 @@
 package epicfitpc;
-/*
-import java.awt.EventQueue;
 
+import java.awt.EventQueue;
+/*
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
@@ -11,9 +11,8 @@ import com.google.cloud.firestore.Firestore;
 import epicfitpc.modelo.bbdd.GestorDeUsuarios;
 import epicfitpc.modelo.pojos.Usuario;
 import epicfitpc.utils.Conexion;
-
-import epicfitpc.vista.MainFrame;
 */
+import epicfitpc.vista.MainFrame;
 
 // Esta clase debe lanzar la aplicación
 public class EpicFitPC {
