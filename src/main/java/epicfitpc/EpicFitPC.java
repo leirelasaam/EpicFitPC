447febd3e8106f19c49900abc5ffd88ddcfb33be
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2024-10-19 10:49:11 +0200

    Comentar la prueba realizada

--- src/main/java/epicfitpc/EpicFitPC.java
+++ src/main/java/epicfitpc/EpicFitPC.java
@@ -1,6 +1,7 @@
 package epicfitpc;
 
 import java.awt.EventQueue;
+/*
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
@@ -10,6 +11,7 @@ import com.google.cloud.firestore.Firestore;
 import epicfitpc.modelo.bbdd.GestorDeUsuarios;
 import epicfitpc.modelo.pojos.Usuario;
 import epicfitpc.utils.Conexion;
+*/
 import epicfitpc.vista.MainFrame;
 
 // Esta clase debe lanzar la aplicación
