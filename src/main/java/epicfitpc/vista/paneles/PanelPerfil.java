dd3bba43b5cdb895ecc358df82e546e93e22b490
Violeta <violeta.uriengr@elorrieta-errekamari.com>
2024-10-30 17:54:13 +0100

    Creación ventana panel de perfil.

--- src/main/java/epicfitpc/vista/paneles/PanelPerfil.java
+++ src/main/java/epicfitpc/vista/paneles/PanelPerfil.java
@@ -3,6 +3,8 @@ package epicfitpc.vista.paneles;
 import javax.swing.JPanel;
 
 public class PanelPerfil extends JPanel {
+	public PanelPerfil() {
+		setLayout(null);
+	}
 	private static final long serialVersionUID = -809466126072228019L;
-
 }
